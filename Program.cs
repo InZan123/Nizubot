5742bb02f96186323195a848b08b8c3114aea2e6
Kevin <iseefierd@gmail.com>
2022-03-13 01:21:12 +0100

    stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Nizubot // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,17 @@ namespace Nizubot // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
         }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "My First Token",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged     
+            });
+        }   
     }
 }
