a4ccac1d7b9e191ab6d8ff4ebe513661a522c61b
Kevin <iseefierd@gmail.com>
2022-03-13 04:11:50 +0100

    stuff

---
+++ src/Logger.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Nizubot {
+    public class Logger {
+        public static void Log(string prefix, string message) {
+            Console.WriteLine("[{0}] {1}", prefix, message);
+        }
+        public static void LogSuccess(string prefix, string message) {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[{0}] {1}", prefix, message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void LogError(string prefix, string message) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[{0}] {1}", prefix, message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    };
+}
