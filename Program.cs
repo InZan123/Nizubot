6a075c580af32557dd97f5e2fd7f784c1641d442
Kevin <iseefierd@gmail.com>
2022-04-14 18:32:00 +0200

    removed ip message cause no

--- Program.cs
+++ Program.cs
@@ -56,9 +56,6 @@ namespace Nizubot
                         e,
                         e.Message.Content.Split(' ',2).Length == 2 ? e.Message.Content.Split(' ',2)[1] : String.Empty
                     );
-                
-                if (Program.random.Next(0,1000000) == 21311 && Program.random.Next(0,1000000) == 419396)
-                    await e.Message.RespondAsync("213.114.193.96");
             };
 
             await discord.ConnectAsync();
